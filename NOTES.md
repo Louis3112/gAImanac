4de9bf19733b8a1d0b4ad312f5929588f88c7286
Louis <corneliuslouis3112@gmail.com>
2025-06-28 15:15:42 +0700

    Add NOTES.md

---
+++ NOTES.md
@@ -0,0 +1,57 @@
+# :bangbang: **Software Development Project for 4th Semester**  :bangbang:
+
+##  🤖 :iphone: **gAImanac** 🤖 :iphone:
+gAImanac *(prononunced galmanac, not gaymanac)* is an AI Agent designed to assist users in the gaming industry ecosystem (let's just say it is a gaming chatbot AI)
+Derived from the words 'Gaming' and 'Almanac', gAImanac aims to be a reliable source information about gaming news, gaming tips, and other gaming-related content 
+
+At this stage, gAImanac is still on conseptual design. It is an idea that popped out for our software development project
+In the future, with enough knowledge, resources, and team. We can make this dream come true.
+
+Tbh, this is my first time using react.js as a framework. 
+At first, @rayhan889, @pachyyy, and I created gAImanac only by using Figma (https://www.figma.com/design/zOmyobzUCKzkPp2zUuLF9x/UTS-RPL---Chatbot?node-id=0-1&t=mOvA40NX0MpkEbQr-1)
+However, when we saw one of my friend has implemented his project as a full website, we decided to take a serious approach.
+
+Since we only have 2 days left, i barely have time to understand how to use a framework (on this moment, i don't know anything about routes, components, let alone state management)
+So, i I ended up creating the entire main page directly in `App.tsx` -- all at once.
+
+I know this is funny and I can improve it, but i want to save it as a memory.      
+
+Framework i am using are : 
+1. React.js for website framework
+2. Shadcn/ui for button and avatar component (and yes, i don't know what is component back then). 
+3. Tailwind CSS for CSS 
+
+The primary color palette i am using are :
+1. #09090B (Zinc - 950)
+2. #18181B (Zinc - 900)
+3. #27272A (Zinc - 800)
+4. #3F3F46 (Zinc - 700)
+5. #52525C (Zinc - 600)
+5. #FFFFFF (White)
+6. #162456 (Blue - 900)
+7. #1447E6 (Blue - 700)
+8. #016630 (Green - 800)
+
+## :arrow_forward: **Installation and Usage** 
+To install this project, please proceed these steps:
+1. Clone the repository: **`git clone https://github.com/Louis3112/gaimanac`**
+2. Navigate to the project directory: **`cd gaimanac`**
+3. Type **`npm i`** to install node_modules
+4. Run **`npm run dev`** to start the development server on localhost
+5. Vite will start the development server and there will be a link **`Local: http://localhost:YOUR_PORT`** -- click it to open the site
+6. Your default browser will open the site automatically
+
+## 	:bust_in_silhouette: **Contributing** :bust_in_silhouette:
+If you'd like to contribute or upgrade the project, it would be my pleasure! :smile: 
+
+Here's how to to contribute:
+1. Fork the repository
+2. Create a new branch: **`git checkout -b your-branch-name`**
+3. Make your changes
+4. Commit your changes: **`git commit -m "your messages"`** 
+5. Push your changes to your forked repository: **`git push origin your-branch-name`**
+6. Submit a pull request
+
+## :telephone_receiver: **Contact** :telephone_receiver:
+
+If you have any questions or comments about this project, please contact **[me](corneliuslouis3112@gmail.com)**
--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
 const App = () => {
   return (
-    <div className="min-h-screen flex text-white bg-[#09090B]">
+    <div className="min-h-screen flex text-white bg-zinc-800">
 
       {/* Left Sidebar */}
-      <aside className="w-[280px] h-screen p-6 flex flex-col space-y-6 justify-between bg-[#09090B]">
+      <aside className="w-[280px] h-screen p-6 flex flex-col space-y-6 justify-between bg-zinc-950">
         
         {/* Sidebar Header */}
         <div className="flex items-center justify-between w-full">
@@ -66,7 +66,7 @@ const App = () => {
             </div>
           </div>
 
-          <div className="flex justify-between mt-4 pt-4 border-t border-[#2e2e32]">
+          <div className="flex justify-between mt-4 pt-4 border-t border-zinc-700">
             <a href="#" className="text-sm text-white mt-1">Log out</a>
             <Button className="cursor-pointer" variant={"ghost"}>
               <LogOut/>
@@ -76,14 +76,14 @@ const App = () => {
       </aside>
 
       <main id="main-content" className="flex-1 flex items-center justify-center">
-        <div className="w-full min-w-sm max-w-4xl rounded-xl p-6 shadow-lg space-y-14 bg-[#1F1F24]">
+        <div className="w-full min-w-sm max-w-4xl rounded-xl p-6 shadow-lg space-y-14 bg-zinc-900">
           
           {/* Chatbot Header */}
           <div id="chat-header" className="flex flex-col gap-0">
           <div className="flex items-center justify-between">
             <h2 className="text-2xl">AI Chat</h2>
             <div className="flex items-center gap-4">
-              <Button variant={"ghost"} className="cursor-pointer bg-[#27272A]">
+              <Button variant={"ghost"} className="cursor-pointer bg-zinc-700">
                 <Info className="opacity-60"/>
               </Button>
               <Avatar>
@@ -92,7 +92,7 @@ const App = () => {
               </Avatar>
             </div>
           </div>
-          <hr className="border-[#3F3F46] mt-4 w-full" />
+          <hr className="border-zinc-700 mt-4 w-full" />
         </div>
           
           {/* Chatbot Body */}
@@ -104,7 +104,7 @@ const App = () => {
                 <AvatarImage src={profilePict} />
                 <AvatarFallback>CN</AvatarFallback>
               </Avatar>
-              <div className="p-4 rounded-lg bg-[#27272A] max-w-2xl">
+              <div className="p-4 rounded-lg bg-zinc-800 max-w-2xl">
                 <p className="font-bold text-white mb-2">You</p>
                 <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea non sequi quaerat, dolor velit aspernatur ut facilis quia iusto veniam recusandae reiciendis deserunt, accusamus exercitationem blanditiis. Ducimus vitae dolorem sunt aut culpa sequi esse quidem ipsam sapiente suscipit eveniet ut officia animi autem nisi inventore ea quo blanditiis voluptatibus repudiandae, tempora magni voluptatum! Vitae, cum expedita atque, minima asperiores dolorem nemo impedit possimus quas animi, dolores eos aliquid optio perspiciatis modi odit rem! Beatae quam nisi ab, nesciunt amet repellendus quae hic laboriosam corrupti et iusto? Dicta ipsum dolorum alias recusandae? Deserunt, sit impedit explicabo optio nisi veniam fugit ducimus.</p>
               </div>
@@ -114,17 +114,17 @@ const App = () => {
             {/* Bot Message */}
             <div className="flex items-start justify-start gap-3">
               <img src={gAImanacIcon} alt="gAImanac Icon" className="self-end w-8 h-8" />
-              <div className="p-4 rounded-lg bg-[#1C398E] max-w-2xl">
+              <div className="p-4 rounded-lg bg-blue-900 max-w-2xl">
                 <p className="font-bold text-white mb-2">gAImanac</p>
                 <p className="text-sm">Welcome Adventurer, what can I do for you?</p>
               </div>
             </div>
 
           {/* Chatbot Footer */}
-          <div id="chat-footer" className="pt-4 border-t border-[#2e2e32]">
+          <div id="chat-footer" className="pt-4 border-t border-zinc-700">
             <form action="#" className="flex items-center gap-3">
-              <input type="text" placeholder="Start prompting, and we do the rest👌" className="flex-1 p-3 rounded-lg bg-[#27272A] text-white text-sm placeholder-opacity-50" required />
-              <Button className="cursor-pointer bg-[#1C398E] rounded-lg hover:bg-[#3055e0] transition">
+              <input type="text" placeholder="Start prompting, and we do the rest👌" className="flex-1 p-3 rounded-lg bg-zinc-800 text-white text-sm placeholder-opacity-50" required />
+              <Button className="cursor-pointer bg-blue-900 rounded-lg hover:bg-blue-700 transition">
                 <Send/>
               </Button>
             </form>
@@ -140,17 +140,17 @@ const App = () => {
       </main>
 
       {/* Right Sidebar */}
-      <aside className="w-[280px] h-screen p-6 flex flex-col space-y-8 justify-between bg-[#09090B]">
+      <aside className="w-[280px] h-screen p-6 flex flex-col space-y-8 justify-between bg-zinc-900">
         
         <div>
           {/* Sidebar Header */}
           <div className="flex justify-between mt-2">
             <h1 className="text-xl text-white">History</h1>
-            <div className="px-3 py-1 rounded-md bg-[#27272A] text-white text-sm">
+            <div className="px-3 py-1 rounded-md bg-zinc-800 text-white text-sm">
               <p>3/25</p> {/* current/ max list */}
             </div>
           </div> 
-          <hr className="border-[#3F3F46] mt-5 w-full"/>
+          <hr className="border-zinc-700 mt-5 w-full"/>
         </div>
         
         {/* List History */}
@@ -159,7 +159,7 @@ const App = () => {
             {/* Square */}
             <label className="absolute w-6 h-6 border-2 rounded-sm cursor-pointer border-zinc-600 ">
               <input type="checkbox" className="peer hidden" />
-              <div className="w-full h-full hover:bg-[#27272A] peer-checked:bg-[#E4E4E7] transition"></div>
+              <div className="w-full h-full hover:bg-zinc-600 peer-checked:bg-white transition"></div>
             </label>
             
             {/* Name game and question */}
@@ -175,7 +175,7 @@ const App = () => {
             {/* Square */}
             <label className="absolute w-6 h-6 border-2 rounded-sm cursor-pointer border-zinc-600 ">
               <input type="checkbox" className="peer hidden" />
-              <div className="w-full h-full hover:bg-[#27272A] peer-checked:bg-[#E4E4E7] transition"></div>
+              <div className="w-full h-full hover:bg-zinc-600  peer-checked:bg-white transition"></div>
             </label>
             
             {/* Name game and question */}
@@ -191,7 +191,7 @@ const App = () => {
             {/* Square */}
             <label className="absolute w-6 h-6 border-2 rounded-sm cursor-pointer border-zinc-600 ">
               <input type="checkbox" className="peer hidden" />
-              <div className="w-full h-full hover:bg-[#27272A] peer-checked:bg-[#E4E4E7] transition"></div>
+              <div className="w-full h-full hover:bg-zinc-600 peer-checked:bg-white transition"></div>
             </label>
             
             {/* Name game and question */}
@@ -205,7 +205,7 @@ const App = () => {
         </ul>
 
         <div className="flex justify-center">
-          <Button className="cursor-pointer w-full bg-[#27272A]" variant={"ghost"}>
+          <Button className="cursor-pointer w-full bg-zinc-800" variant={"ghost"}>
             <Trash/>
             <p>Clear History</p>
           </Button>
